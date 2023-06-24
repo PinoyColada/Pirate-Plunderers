53cf2b883a7965072559e13ceca85c020536ebf1
pinoycolada <pinoydol.colada@gmail.com>
2023-06-23 21:36:27 -0400

    styled and added back button

--- CMSC 495/pirate-plunderers/src/components/BackButton.jsx
+++ CMSC 495/pirate-plunderers/src/components/BackButton.jsx
@@ -1,11 +1,12 @@
 import { useNavigate } from "react-router-dom";
+import photo from "../images/back_button.png"
 
 const BackButton = () => {
     const navigate = useNavigate();
 
     return (
-        <button id="button-container" onClick={() => navigate('/')}>
-            Back
+        <button id="button-container">
+            <img className="back-button" onClick={() => navigate('/')} src={photo} alt="back button"/>
         </button>
     )
 }
