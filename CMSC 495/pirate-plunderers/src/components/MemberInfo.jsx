cfefc8630e6cc59579cbdca52218098a98ad02a0
pinoycolada <pinoydol.colada@gmail.com>
2023-06-27 04:42:11 -0400

    added icons and fixed styling

--- CMSC 495/pirate-plunderers/src/components/MemberInfo.jsx
+++ CMSC 495/pirate-plunderers/src/components/MemberInfo.jsx
@@ -1,9 +1,15 @@
+import { FaGithub, FaLinkedin } from 'react-icons/fa6';
+
 const MemberInfo = (props) => {
     return (
         <div id="profile-section">
             <img src={props.image} alt={`${props.image} headshot`} />
             <h3>{props.name}</h3>
             <h4>{props.role}</h4>
+            <div id='buttons-container'>
+                <FaGithub size='2rem' />
+                <FaLinkedin size='2rem' />
+            </div>
         </div>
     )
 }
